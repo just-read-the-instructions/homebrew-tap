402eb12796d8a21f506d243895a69d357ace613b
raspla <34218225+raspla@users.noreply.github.com>
2024-11-20 11:40:54 +0100

    Update solr to 9.7.0
    Bump solr to new major version

--- Formula/solr.rb
+++ Formula/solr.rb
@@ -1,24 +1,22 @@
 class Solr < Formula
   desc "Enterprise search platform from the Apache Lucene project"
   homepage "https://solr.apache.org/"
-  url "https://dlcdn.apache.org/lucene/solr/8.11.2/solr-8.11.2.tgz"
-  mirror "https://archive.apache.org/dist/lucene/solr/8.11.2/solr-8.11.2.tgz"
-  sha256 "54d6ebd392942f0798a60d50a910e26794b2c344ee97c2d9b50e678a7066d3a6"
+  url "https://dlcdn.apache.org/solr/solr/9.7.0/solr-9.7.0.tgz"
+  mirror "https://archive.apache.org/dist/solr/solr/9.7.0/solr-9.7.0.tgz"
+  sha256 "38548b86fa4e3c87883875952da124bf7d742cb8f7b25d37a1176833588e8552"
   license "Apache-2.0"
 
   def install
     pkgshare.install "bin/solr.in.sh"
-    (var/"lib/solr").install "server/solr/README.txt", "server/solr/solr.xml", "server/solr/zoo.cfg"
-    prefix.install %w[contrib dist server]
-    libexec.install "bin"
-    bin.install [libexec/"bin/solr", libexec/"bin/post", libexec/"bin/oom_solr.sh"]
+    (var/"lib/solr").install "server/solr/README.md", "server/solr/solr.xml", "server/solr/zoo.cfg"
+    prefix.install "licenses", "modules", "server"
+    bin.install "bin/solr", "bin/post"
 
     env = Language::Java.overridable_java_home_env
-    env["SOLR_HOME"] = "${SOLR_HOME:-#{var/"lib/solr"}}"
-    env["SOLR_LOGS_DIR"] = "${SOLR_LOGS_DIR:-#{var/"log/solr"}}"
-    env["SOLR_PID_DIR"] = "${SOLR_PID_DIR:-#{var/"run/solr"}}"
+    env["SOLR_HOME"] = "${SOLR_HOME:-#{var}/lib/solr}"
+    env["SOLR_LOGS_DIR"] = "${SOLR_LOGS_DIR:-#{var}/log/solr}"
+    env["SOLR_PID_DIR"] = "${SOLR_PID_DIR:-#{var}/run/solr}"
     bin.env_script_all_files libexec, env
-    (libexec/"bin").rmtree
 
     inreplace libexec/"solr", "/usr/local/share/solr", pkgshare
   end
@@ -28,21 +26,27 @@ class Solr < Formula
     (var/"log/solr").mkpath
   end
 
+  service do
+    run [opt_bin/"solr", "start", "-f", "-s", HOMEBREW_PREFIX/"var/lib/solr"]
+    working_dir HOMEBREW_PREFIX
+  end
+
   test do
     ENV["SOLR_PID_DIR"] = testpath
     port = free_port
 
-    # Info detects no Solr node => exit code 3
-    shell_output(bin/"solr -i", 3)
+    assert_match "No Solr nodes are running", shell_output("#{bin}/solr status")
+
     # Start a Solr node => exit code 0
-    shell_output(bin/"solr start -p #{port} -Djava.io.tmpdir=/tmp")
-    # Info detects a Solr node => exit code 0
-    shell_output(bin/"solr -i")
+    shell_output("#{bin}/solr start -p #{port} -Djava.io.tmpdir=/tmp")
+    assert_match "Found 1 Solr nodes", shell_output("#{bin}/solr status")
+
     # Impossible to start a second Solr node on the same port => exit code 1
-    shell_output(bin/"solr start -p #{port}", 1)
+    shell_output("#{bin}/solr start -p #{port}", 1)
     # Stop a Solr node => exit code 0
-    shell_output(bin/"solr stop -p #{port}")
+    # Exit code is 1 without init process in a docker container
+    shell_output("#{bin}/solr stop -p #{port}", (OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]) ? 1 : 0)
     # No Solr node left to stop => exit code 1
-    shell_output(bin/"solr stop -p #{port}", 1)
+    shell_output("#{bin}/solr stop -p #{port}", 1)
   end
 end
